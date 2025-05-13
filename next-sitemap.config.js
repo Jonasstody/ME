8d10c9bd866d3c285ab0149d242e45cdcdf329ad
Jonas F <randommail0203@gmail.com>
2025-05-13 15:53:08 +0000

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    siteUrl: 'https://jonasfabiankoerl.me',
+    generateRobotsTxt: true,
+    outDir: './public',
+    // Since this is a one-page site
+    exclude: ['/*'],
+    include: ['/']
+  }
